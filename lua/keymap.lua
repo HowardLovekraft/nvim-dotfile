2d69cf772a01b9da117faf71952492ba5b3a59c3
HowardLovekraft <117375241+HowardLovekraft@users.noreply.github.com>
2026-07-08 18:47:18 +0500

    refactor keymap

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -1,26 +1,12 @@
-local function reload_config()
-	-- Clear cache for custom Lua files
-	for name, _ in pairs(package.loaded) do
-		package.loaded[name] = nil
-	end
-
-	dofile(vim.env.MYVIMRC)
-	vim.notify("Configuration reloaded!", vim.log.levels.INFO)
-end
-
--- Hot Reload конфига nvim
-vim.keymap.set("n", "<leader>sv", reload_config, { desc = "Hot reload neovim's config" })
-
--- Запускает Netrw
--- vim.keymap.set("n", "<leader>e", "<Cmd>Explore<CR>")
+-- Открывает Neotree
+vim.keymap.set("n", "<leader>e", "<Cmd>Neotree<CR>")
 
 -- Кеймапы для fzf
 local fzf = require("fzf-lua")
-
-vim.keymap.set("n", "<leader>f", fzf.files)
+vim.keymap.set("n", "<leader>fs", fzf.files)
 vim.keymap.set("n", "<leader>/", fzf.live_grep)
 
--- Кеймапы для Lua LSP
+-- Кеймапы для LSP
 local opts = { noremap = true, silent = true }
 
 vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
@@ -28,13 +14,16 @@ vim.keymap.set("n", "<Leader>fo", "<cmd>lua vim.lsp.buf.format()<CR>", opts)
 
 
 -- Кеймап для which-key
-vim.keymap.set("n", "<leader>?", 
+vim.keymap.set("n", "<leader>?",
   function()
-    require("which-key").show{ global = false }
+    require("which-key").show { global = false }
   end,
-  { 
+  {
     noremap = true,
-	silent = true,
-	desc = "Buffer Local Keymaps (which-key)", 
+    silent = true,
+    desc = "Buffer Local Keymaps (which-key)",
   }
 )
+
+-- Удобный выход из Terminal Mode
+vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", opts)
